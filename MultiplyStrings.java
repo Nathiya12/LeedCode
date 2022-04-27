b9abcff748eca889e25724c45848632e1d62b8c8
Nathiya G <100260155+Nathiya12@users.noreply.github.com>
2022-04-27 12:29:50 +0530

    Create MultiplyStrings.java

---
+++ MultiplyStrings.java
@@ -0,0 +1,5 @@
+  public String multiply(String num1, String num2) {
+    BigInteger in = new BigInteger(num1);
+    BigInteger in2 = new BigInteger(num2);
+      return in.multiply(in2).toString();
+    }
